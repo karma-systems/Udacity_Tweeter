01eda5dbb7d27836f1c8ee5a3436a21e405cb0d8
Ashraf Kamel <a.kamel@karma-systems.com>
2021-11-24 13:14:24 +0200

    Bootstrap

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -7,6 +7,7 @@ import { formatTweet, formatDate } from '../utils/helpers'
 //import {TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/lib'
 import {TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -22,7 +23,7 @@ class Tweet extends Component {
   }
   toParent = (e, id) => {
     e.preventDefault()
-    // todo: Redirect to parent Tweet.
+    this.props.history.push(`/tweet/${id}`)
   }
   render() {
     const { tweet } = this.props
@@ -32,11 +33,11 @@ class Tweet extends Component {
     }
 
     const {
-      name, avatar, timestamp, text, hasLiked, likes, replies, parent
+      name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
     } = tweet
 
     return (
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img
           src={avatar}
           alt={`Avatar of ${name}`}
@@ -64,7 +65,7 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -81,4 +82,4 @@ function mapStateToProps ({authedUser, users, tweets}, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
+//import 'bootstrap/dist/bootstrap.min.css';
 
 const store = createStore(reducer, middleware)
 
 ReactDOM.render(
 <Provider store={store}>
-<App />
+    <App />
 </Provider>, 
 document.getElementById('root'))
